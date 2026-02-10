53922bc68ca78abc0fbd36b5b49d28f17044a461
uttam <uttamacharya520@gmail.com>
2026-02-11 03:06:03 +0530

    add without cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,48 +9,48 @@ import routes from "./routes/index.route.js"
 const app = express()
 
 /*Debug logger (safe) */
-app.use((req, res, next) => {
-  console.log("REQ:", req.method, req.url, "ORIGIN:", req.headers.origin);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("REQ:", req.method, req.url, "ORIGIN:", req.headers.origin);
+//   next();
+// });
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      // health checks / curl / server-to-server
-      if (!origin) return callback(null, true);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       // health checks / curl / server-to-server
+//       if (!origin) return callback(null, true);
 
-      const allowedOrigins = [
-        "http://localhost:5173",                 // local
-        "https://fin-sight-chi-coral.vercel.app", // prod
-      ];
+//       const allowedOrigins = [
+//         "http://localhost:5173",                 // local
+//         "https://fin-sight-chi-coral.vercel.app", // prod
+//       ];
 
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
+//       if (allowedOrigins.includes(origin)) {
+//         return callback(null, true);
+//       }
 
-      return callback(null, false); // do NOT throw error
-    },
-    credentials: true,
-  })
-);
+//       return callback(null, false); // do NOT throw error
+//     },
+//     credentials: true,
+//   })
+// );
 
-app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization"
-    );
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS"
-    );
-    return res.sendStatus(204);
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Origin", req.headers.origin);
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Content-Type, Authorization"
+//     );
+//     res.header(
+//       "Access-Control-Allow-Methods",
+//       "GET, POST, PUT, DELETE, OPTIONS"
+//     );
+//     return res.sendStatus(204);
+//   }
+//   next();
+// });
 
 // app.use(
 //   helmet({
