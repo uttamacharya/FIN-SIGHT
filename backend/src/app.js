9d3bb5b01d627ca50c22675e6e4bb8b68711281a
uttam <uttamacharya520@gmail.com>
2026-02-10 16:39:54 +0530

    improve url

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,20 +8,24 @@ import routes from "./routes/index.route.js"
 
 const app = express()
 
-const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
+const allowedOrigins = [
+  process.env.FRONTEND_URL?.replace(/\/$/, ""),
+  "http://localhost:5173",
+];
 
 app.use(cors({
-  origin:(origin, callback) =>{
-    if (!origin) return callback(null, true)
-    if(allowedOrigins.includes(origin)){
-      return callback(null, origin);
-    } 
-    return callback(new Error("Not allowed by CORS"))
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+
+    const cleanOrigin = origin.replace(/\/$/, "");
+    if (allowedOrigins.includes(cleanOrigin)) {
+      return callback(null, true);
+    }
+    return callback(new Error("Not allowed by CORS"));
   },
   credentials: true,
-  methods:["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
 }));
+
 
 app.use(helmet())
 app.use(morgan("dev"))
